b86fdaf7736861b316abd8ffa83a35507dd221d0
hemantpandey-nift <hemantpandey.nift@gmail.com>
2024-01-22 14:30:15 +0530

    data handling fix

--- src/user/dto/duplicateValueConstraint.ts
+++ src/user/dto/duplicateValueConstraint.ts
@@ -6,6 +6,7 @@ import {
 @ValidatorConstraint({ name: 'hasDuplicateValues' })
 export class HasDuplicateValues implements ValidatorConstraintInterface {
   validate(data: string): boolean {
+    if (!data) return false;
     return Array.from(new Set(data)).length === data.length ? true : false;
   }
 }
